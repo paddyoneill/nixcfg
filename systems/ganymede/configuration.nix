e4382c1537c66f61a1d5095bbab50f2f501f899c
Patrick O'Neill <paddy.oneill93@gmail.com>
2023-05-08 21:23:44 +0100

    Add hostname to ganymede configuration

--- systems/ganymede/configuration.nix
+++ systems/ganymede/configuration.nix
@@ -6,6 +6,9 @@
       ./hardware-configuration.nix
     ];
 
+  # Network configuration
+  networking.hostName = "ganymede";
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
