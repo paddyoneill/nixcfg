8d268d278bd4b6431e214ee2d70a116187c23697
Patrick O'Neill <paddy.oneill93@gmail.com>
2023-05-08 21:24:29 +0100

    Update timezone information

--- systems/ganymede/configuration.nix
+++ systems/ganymede/configuration.nix
@@ -9,6 +9,10 @@
   # Network configuration
   networking.hostName = "ganymede";
 
+  # Location info
+  time.timeZone = "Europe/London";
+  location.provider = "geoclue2";
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
