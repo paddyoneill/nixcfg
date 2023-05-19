20046d35d054cc1ae34723bb54b8ccbb34cb255a
Patrick O'Neill <paddy.oneill93@gmail.com>
2023-05-19 16:32:35 +0100

    Remove unnecessary directories

--- configuration.nix
+++ configuration.nix
@@ -26,6 +26,11 @@
     keyMap = "uk";
   };
 
+  users.users.paddy = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
